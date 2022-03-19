67d80587ce5a19704096b0390c86a4d941774841
Pattarawut <98207051+PattarawutSyn@users.noreply.github.com>
2022-03-19 16:36:43 +0700

    class/10/ex01 for 1630700480

--- src/components/Chatline.js
+++ src/components/Chatline.js
@@ -18,7 +18,7 @@ function Chatline(props) {
       const [, name] = text.split(" ");
       fetch("https://api.agify.io/?name=" + name)
         .then(res => res.json())
-        .then(data => setElement(data.age));
+        .then(data => setElement(data.name + " : " +name));
     }
     else if (text.startsWith("/gender")) {
       const [, name] = text.split(" ");
@@ -29,7 +29,16 @@ function Chatline(props) {
     else if (text.startsWith("/randomfact")) {
       // ex10-01 here: Implement randomfact using fetch & promise
       // https://uselessfacts.jsph.pl/random.json?language=en
-
+      fetch("https://uselessfacts.jsph.pl/random.json?language=en ")
+        .then(res => res.json())
+        .then(data => setElement(data.text||"c"));
+    }
+    else if (text.startsWith("/nationalize")) {
+      const [, name] =  text.split(" ");
+      fetch("https://api.nationalize.io/?name=" + name)
+        .then(res => res.json())
+        .then(data => setElement(data.country))
+        .catch(err => setElement("พัง"))
     }
   };
 
